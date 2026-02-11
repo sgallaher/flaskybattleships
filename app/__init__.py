37a2777c43011d1cb3607b04c3949d99180ac9f2
sgallaher <54886009+sgallaher@users.noreply.github.com>
2026-02-11 08:24:43 +0000

    first save of files and structure

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .extensions import db, socketio, login_manager
+
+def create_app():
+    app= Flask(__name__)
+    app.config.from_object("config.Congig")
+
+    db.init_app(app)
+    socketio.init_app(app, cors_allowed_origins="*")
+    login_manager.init_app(app)
+
+    from .routes.auth import auth_bp
+    from .routes.games import game_bp
+    from .sockets.game_socket import register_sockets
+
